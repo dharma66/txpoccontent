dc0737a0b6b8fbb23779a05dfb647384449c935d
Phil Lewis <phil.lewis@sage.com>
2017-01-09 17:16:14 +0000

    Removed redundant import.

--- src/main/java/com/sage/prometheus/poc/TransactionRepository.java
+++ src/main/java/com/sage/prometheus/poc/TransactionRepository.java
@@ -2,10 +2,7 @@ package com.sage.prometheus.poc;
 
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-import java.util.List;
 
 @RepositoryRestResource
 public interface TransactionRepository extends MongoRepository<Transaction, String> {}
